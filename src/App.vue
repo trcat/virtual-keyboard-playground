edf9ed013876a62d3403fca06b36de349de01485
Henry Hu <daydaybigking@sina.com>
2021-07-08 09:49:53 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="input">
+      {{ keyboardValue || '点击虚拟键盘输入内容' }}
+    </div>
+    <SimpleKeyboard
+      @onChange="onChange"
+      @onKeyPress="onKeyPress"
+      :input="input"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SimpleKeyboard from "./SimpleKeyboard";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SimpleKeyboard,
+  },
+  data: () => ({
+    keyboardValue: "",
+  }),
+  methods: {
+    onChange(input) {
+      this.input = input;
+      this.keyboardValue = "";
+    },
+    onKeyPress(button) {
+      console.log("button", button);
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.input {
+  width: 100%;
+  height: 100px;
+  padding: 20px;
+  font-size: 20px;
+  border: none;
+  box-sizing: border-box;
+}
+
+.simple-keyboard {
+  max-width: 850px;
 }
 </style>
